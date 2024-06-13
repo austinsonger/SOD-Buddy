31fa0d23d5dc009e3444114e2bf111fc6297f553
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-06-13 05:30:50 +0000

    Update

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,17 +1,29 @@
-/**
- * Unit tests for the action's entrypoint, src/index.ts
- */
+// index.test.ts
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as fs from 'fs'
+import { parseCodeowners } from './parseCodeowners'
+import { getCommitters } from './getCommitters'
+import { getReviewers } from './getReviewers'
+import { findEligibleApprover } from './findEligibleApprover'
+import { run } from './index'
 
-import * as main from '../src/main'
+jest.mock('@actions/core')
+jest.mock('@actions/github')
+jest.mock('fs')
+jest.mock('./parseCodeowners')
+jest.mock('./getCommitters')
+jest.mock('./getReviewers')
+jest.mock('./findEligibleApprover')
 
-// Mock the action's entrypoint
-const runMock = jest.spyOn(main, 'run').mockImplementation()
+describe('run', () => {
+  it('should call core.setFailed when no pull request number is found', async () => {
+    github.context.payload.pull_request = null
 
-describe('index', () => {
-  it('calls run when imported', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index')
+    await run()
 
-    expect(runMock).toHaveBeenCalled()
+    expect(core.setFailed).toHaveBeenCalledWith('No pull request number found.')
   })
+
+  // Add more tests here for other scenarios...
 })
